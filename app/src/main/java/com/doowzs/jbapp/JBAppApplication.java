cc94fc64c97e3bc5c69df405b4a04c443299f46f
Tianyun Zhang <doowzs@qq.com>
2018-12-09 21:21:01 +0800

    color and drawables update

--- app/src/main/java/com/doowzs/jbapp/JBAppApplication.java
+++ app/src/main/java/com/doowzs/jbapp/JBAppApplication.java
@@ -1,7 +1,6 @@
 package com.doowzs.jbapp;
 
 import android.app.Application;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
